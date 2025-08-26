8cdfaccd5197d8eb5ac19c5584c7dcd91ca6685e
harsh522004 <harshbutani2004@gmail.com>
2025-08-26 21:14:59 +0530

    Re-init

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -30,8 +30,6 @@ contract CrowdFunding {
         compaign.deadline = block.timestamp + durationSeconds;
         compaign.totalRaised = 0;
         compaign.state = State.Funding;
-
-        emit CampaignCreated(compaign.creator, goal, compaign.deadline);
     }
 
     // events
@@ -40,27 +38,4 @@ contract CrowdFunding {
         uint256 goal,
         uint256 deadline
     );
-
-    // Modifiers
-    modifier ownerCheck() {
-        require(
-            msg.sender != compaign.creator,
-            "creator has no rights to contribute in compaign"
-        );
-        _;
-    }
-    modifier stateCheck() {
-        require(
-            compaign.state == State.Funding,
-            "compaign is not under the state FUNDING"
-        );
-        _;
-    }
-    modifier deadlineCheck() {
-        require(block.timestamp < compaign.deadline, "deadline missed!");
-        _;
-    }
-
-    // Contribute Anyone
-    function contribute() public {}
 }

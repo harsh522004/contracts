a504c4b1dfa2bd1ab6e7d7a4d42e023ee9acfc1c
harsh522004 <harshbutani2004@gmail.com>
2025-08-26 21:16:08 +0530

    Re-Init3

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -1,3 +1,4 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
@@ -30,6 +31,8 @@ contract CrowdFunding {
         compaign.deadline = block.timestamp + durationSeconds;
         compaign.totalRaised = 0;
         compaign.state = State.Funding;
+
+        emit CampaignCreated(compaign.creator, goal, compaign.deadline);
     }
 
     // events
@@ -38,4 +41,27 @@ contract CrowdFunding {
         uint256 goal,
         uint256 deadline
     );
+
+    // Modifiers
+    modifier ownerCheck() {
+        require(
+            msg.sender != compaign.creator,
+            "creator has no rights to contribute in compaign"
+        );
+        _;
+    }
+    modifier stateCheck() {
+        require(
+            compaign.state == State.Funding,
+            "compaign is not under the state FUNDING"
+        );
+        _;
+    }
+    modifier deadlineCheck() {
+        require(block.timestamp < compaign.deadline, "deadline missed!");
+        _;
+    }
+
+    // Contribute Anyone
+    function contribute() public {}
 }
